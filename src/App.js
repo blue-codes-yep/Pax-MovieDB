bc05f6a73eba524c5f5ba2f7b48d5ef17c0de0a4
Blue Lindsey <linzblue5@gmail.com>
2020-04-16 12:09:17 -0500

    Setting up routes, to bounce between home, and movie details using React Router to keep the page from refreshing.

--- src/App.js
+++ src/App.js
@@ -2,40 +2,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from "./images/PAXMDB.svg";
-import Movie from './components/Movie';
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import MovieList from './components/MovieList';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 
-class App extends Component {
+const App = () => (
+  <Router>
+    <div className="App" >
+      <header className="App-header">
+        <Link to="/">
+          <img src={logo} className="App-logo" alt="logo" />
+        </Link>
+      </header>
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/test" component={Test} />
+      </Switch>
+    </div>
+  </Router>
+);
 
-  state = {
-    movies: [],
-  }
+const Test = () => (
+  <h1>test</h1>
+);
 
-  async componentDidMount() {
-    try {
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9e0f685feeb8c6b73e1db5af73029cec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
-      const movies = await res.json();
-
-      this.setState({
-        movies: movies.results,
-      });
-    } catch (e) {
-      console.log(e);
-    }
-  }
-
-  render() {
-    return (
-      <Router>
-        <div className="App" >
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-          </header>
-          {this.state.movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
-        </div>
-      </Router>
-    );
-  }
-}
-
-export default App
+export default App;
--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 
 const image = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
     <div>
-        <img src={`${image}${movie.poster_path}`} alt={movie.title} />
+        <Link to="/test">
+            <img src={`${image}${movie.poster_path}`} alt={movie.title} />
+        </Link>
     </div>
 )
 
