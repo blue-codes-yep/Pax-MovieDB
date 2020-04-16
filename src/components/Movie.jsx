cf77a3a8791841a7e8969ecab3ebb6ce6e41358b
Blue Lindsey <linzblue5@gmail.com>
2020-04-16 12:33:50 -0500

    Added Proptypes for error handeling.

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -7,18 +7,19 @@ const image = 'http://image.tmdb.org/t/p/w154';
 
 const Movie = ({ movie }) => (
     <div>
-        <Link to="/test">
+        <Link to={`/${movie.id}`}>
             <img src={`${image}${movie.poster_path}`} alt={movie.title} />
         </Link>
     </div>
 )
 
 
-export default Movie;
-
 Movie.propTypes = {
     movie: PropTypes.shape({
         title: PropTypes.string.isRequired,
     }).isRequired,
 }
+
+export default Movie;
+
 
