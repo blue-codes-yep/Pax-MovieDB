60ead11f211ef3f0c7a5f909faebae3c8713acb1
Blue Lindsey <linzblue5@gmail.com>
2020-04-16 01:40:46 -0500

    Added logo onto site.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from "./images/PAXMDB.svg"
 import Welcome from './components/welcome'
 
+// const movies = [
+//   {id : 1,
+//   title: 'Star wars'}
+// ]
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.textInput = React.createRef();
-    this.state = {
-      value: ''
-    }
-  }
-  submit = () => {
-    console.log(this.textInput.current.value)
-    this.setState({ value: this.textInput.current.value })
-  }
-
   render() {
     return (
       <div className="App" >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            <Welcome text="Welcome to using props" />
-            <h3>Value: {this.state.value}</h3>
-          </p>
-          <input type="text" ref={this.textInput} />
-          <button onClick={this.submit}>Show Value</button>
         </header>
       </div>
     );
