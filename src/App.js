68d6dadc78caa8a70e182e05634f562ce025a235
Blue Lindsey <linzblue5@gmail.com>
2020-04-16 03:16:11 -0500

    Started using Eslint, and Refactored App with lint.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/state-in-constructor */
 import React, { Component } from 'react';
 import './App.css';
 import logo from "./images/PAXMDB.svg"
@@ -7,18 +8,17 @@ import Movie from './components/Movie'
 class App extends Component {
 
   state = {
-    movies: []
+    movies: [],
   }
 
   async componentDidMount() {
     try {
-
-      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9e0f685feeb8c6b73e1db5af73029cec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9e0f685feeb8c6b73e1db5af73029cec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1');
       const movies = await res.json();
 
       this.setState({
         movies: movies.results
-      })
+      });
     } catch (e) {
       console.log(e);
     }
