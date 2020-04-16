60fd23d41d9bf9d8b8b2e408a42f9aaa4a86b307
Blue Lindsey <linzblue5@gmail.com>
2020-04-16 02:52:08 -0500

    Hitting movie db api

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from "./images/PAXMDB.svg"
-
 import Movie from './components/Movie'
 
-const movies = [
-  {
-    id: 1,
-    title: 'Star wars'
-  },
-  {
-    id: 2,
-    title: 'Spider man'
-  },
-  {
-    id: 3,
-    title: 'Donkey man'
-  }
-]
 
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  async componentDidMount() {
+    try {
+
+      const res = await fetch('https://api.themoviedb.org/3/discover/movie?api_key=9e0f685feeb8c6b73e1db5af73029cec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1')
+      const movies = await res.json();
+
+      this.setState({
+        movies: movies.results
+      })
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   render() {
     return (
       <div className="App" >
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        {movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
+        {this.state.movies.map(movie => (<Movie key={movie.id} movie={movie} />))}
       </div>
     );
   }
